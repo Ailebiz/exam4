5cf9b39bfbf4ee66f9ceba593d2e3b3316710e55
Ailebiz <bolkyzyaliza@gmail.com>
2024-11-11 17:26:39 +0500

    изменил var

---
+++ JS/test4.js
@@ -0,0 +1,83 @@
+// task2
+// var a = +prompt (" Бірінші санды еңгізіңіз:")
+// var b = +prompt (" Екінші санды  еңгізіңіз:")
+// if ( a > b ) {
+//     alert("a-саны b Санынан үлкен" )
+// } 
+// else if ( a < b ) {
+//     alert("a-саны b Санынан кішкентай")
+// } 
+//  else if ( a == b ) {
+//     alert ("Сандар тең")
+// } 
+// switch (true) {
+//         case ( a >= b ):
+//             alert("a саны b Санынан үлкен" )
+//             break;
+//         case (a <=b):
+//             alert("a саны b Санынан кішкентай")
+//             break;
+//         case (a = b):
+//             alert ("Сандар тең")
+//             break;
+//         //  case (num3 <= num1 && num3 <= num2):
+//         //      maxNumber = num3;
+//         //      break;
+//         // case (num3 <= num1 && num3 <= num2):
+//         //      minNumber = num3;
+//         //      break;
+//         //  case (num3 <= num1 && num3 <= num2):
+//         //          minNumber= num3;
+//         //         break;
+//     }
+    
+
+// task-1
+// var mounth=prompt("1-12 aralygynda san engizinis")
+// if (mounth <=3 || mounth <=4 || mounth <=5) {
+//     alert("Spring")
+// } else if (mounth <=6 || mounth <=7 || mounth <=8){
+//     alert("Summer")
+// }else if (mounth <=9 || mounth <=10 || mounth <=11){
+//     alert("Autumn")
+// }else if (mounth <=12 || mounth <=1 || mounth <=2){
+//     alert("Winter")
+// }else{
+//     alert("1 ден 12 ге дейінгі сан енгізіңіз!")
+// }
+
+
+
+
+
+
+// task3
+// var num1= +prompt("Birinshi sandy engiziniz")
+// var num2= +prompt("Ekinshi sandy engiziniz")
+// var num3= +prompt("Ushynshy sandy engiziniz")
+// let maxNumber
+// let minNumber
+// switch (true) {
+//     case (num1 >= num2 && num1 >= num3):
+//         maxNumber = num1;
+//         break;
+//     case (num2 >= num1 && num2 >= num3):
+//         maxNumber = num2;
+//         break;
+//     case (num3 >= num1 && num3 >= num2):
+//         maxNumber = num3;
+//         break;
+//     //  case (num3 <= num1 && num3 <= num2):
+//     //      maxNumber = num3;
+//     //      break;
+//     // case (num3 <= num1 && num3 <= num2):
+//     //      minNumber = num3;
+//     //      break;
+//     //  case (num3 <= num1 && num3 <= num2):
+//     //          minNumber= num3;
+//     //         break;
+// }
+
+
+// alert("Ең үлкен сан: " + maxNumber - minNumber  );
+// // alert("Aiyrymy"= maxNumber - minNumber )
